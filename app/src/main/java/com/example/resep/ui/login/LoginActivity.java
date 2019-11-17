ae355c1678206f696537b1e50e9d652840c3b48b
Feriksan <ferdicool95@gmail.com>
2019-11-17 01:38:23 -0800

    Patch 1.3.1 -removed session for testing purpose -add register class -now application start from RegisterActivity

--- app/src/main/java/com/example/resep/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/resep/ui/login/LoginActivity.java
@@ -54,34 +54,36 @@ public class LoginActivity extends AppCompatActivity {
                 // Get username, password from EditText
                 String username = txtUsername.getText().toString();
                 String password = txtPassword.getText().toString();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
 
                 // Check if username, password is filled
-                if(username.trim().length() > 0 && password.trim().length() > 0){
-                    // For testing puspose username, password is checked with sample data
-                    // username = test
-                    // password = test
-                    if(username.equals("test") && password.equals("test")){
-
-                        // Creating user login session
-                        // For testing i am stroing name, email as follow
-                        // Use user real data
-                        session.createLoginSession("Android Hive", "anroidhive@gmail.com");
-
-                        // Staring MainActivity
-                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(i);
-                        finish();
-
-                    }else{
-                        // username / password doesn't match
-                        alert.showAlertDialog(LoginActivity.this, "Login failed..", "Username/Password is incorrect", false);
-                    }
-                }else{
-                    // user didn't entered username or password
-                    // Show alert asking him to enter the details
-                    alert.showAlertDialog(LoginActivity.this, "Login failed..", "Please enter username and password", false);
-                }
-
+//                if(username.trim().length() > 0 && password.trim().length() > 0){
+//                    // For testing puspose username, password is checked with sample data
+//                    // username = test
+//                    // password = test
+//                    if(username.equals("test") && password.equals("test")){
+//
+//                        // Creating user login session
+//                        // For testing i am stroing name, email as follow
+//                        // Use user real data
+//                        session.createLoginSession("Android Hive", "anroidhive@gmail.com");
+//
+//                        // Staring MainActivity
+//                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+//                        startActivity(i);
+//                        finish();
+//
+//                    }else{
+//                        // username / password doesn't match
+//                        alert.showAlertDialog(LoginActivity.this, "Login failed..", "Username/Password is incorrect", false);
+//                    }
+//                }else{
+//                    // user didn't entered username or password
+//                    // Show alert asking him to enter the details
+//                    alert.showAlertDialog(LoginActivity.this, "Login failed..", "Please enter username and password", false);
+//                }
             }
         });
     }
